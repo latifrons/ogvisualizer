e7e1ba82cc5c47cad693dfd8dd31722c54e30a39
Gan Zejun <ganzejun@zhongan.io>
2019-09-09 18:38:13 +0800

    websocket server

---
+++ script/wsserver.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+# WS server example
+
+import asyncio
+import json
+import time
+from random import Random
+
+import websockets
+
+teams = ['0xAA', '0xBB', '0xCC', '0xDD', '0xEE']
+rando = Random()
+
+
+async def hello(websocket, path):
+    # name = await websocket.recv()
+    # print(f"< {name}")
+    #
+    # greeting = f"Hello {name}!"
+    #
+    # await websocket.send(greeting)
+    # print(f"> {greeting}")
+    tx_hashes = []
+    txs = {}
+    # send sequencer first
+    height = 1
+
+    while True:
+        if len(tx_hashes) != 0:
+            parent_hash = [x for x in set([rando.choice(tx_hashes) for c in range(2)])]
+        else:
+            parent_hash = []
+        t = rando.random()
+        if t < 0.1:
+            tx = {
+                'type': 1,  # seq
+                'hash': rando.randint(0, 1000000),
+                'parent_hash': parent_hash,
+                'from': 'Not used',
+                'to': 'Not used',
+                'nonce': 0,
+                'guarantee': rando.randint(0, 500),
+                'value': 0,
+                'weight': max([txs[x]['weight'] for x in parent_hash]) + 1 if len(parent_hash) != 0 else 0,
+            }
+            height += 1
+        else:
+            tx = {
+                'type': 0,  # tx
+                'hash': rando.randint(0, 1000000),
+                'parent_hash': parent_hash,
+                'nonce': 0,
+                'treasure': rando.randint(0, 500),
+                'height': height,
+                'weight': max([txs[x]['weight'] for x in parent_hash]) + 1 if len(parent_hash) != 0 else 0,
+            }
+
+        tx_hashes.append(tx['hash'])
+        txs[tx['hash']] = tx
+        await websocket.send(json.dumps(tx))
+        time.sleep(1)
+
+
+if __name__ == '__main__':
+    start_server = websockets.serve(hello, "localhost", 8765)
+
+    asyncio.get_event_loop().run_until_complete(start_server)
+    asyncio.get_event_loop().run_forever()
